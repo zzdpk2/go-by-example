c926429ca1d43c0ed1d8d80db4f6cd42003b3c30
zzdpk2 <zzdpk2@126.com>
2025-04-12 22:57:37 +1000

    update

--- practice/range-over-built-in-types/range-over-built-in-types.go
+++ practice/range-over-built-in-types/range-over-built-in-types.go
@@ -16,5 +16,18 @@ func main() {
 			fmt.Println("index: ", i)
 		}
 	}
-	
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Println("key: ", k)
+	}
+
+	// Unicode
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
 }
