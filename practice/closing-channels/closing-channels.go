8dca007f75f9185d8833c581c3165bfdd6bbb40a
zzdpk2 <wotaicanle@gmail.com>
2025-04-27 13:19:58 +1000

    closing channels

--- practice/closing-channels/closing-channels.go
+++ practice/closing-channels/closing-channels.go
@@ -1,1 +1,34 @@
-package closing_channels
+package main
+
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job: ", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Printf("job(%v) sent", j)
+	}
+	close(jobs)
+	fmt.Println("all jobs sent")
+
+	<-done
+
+	_, ok := <-jobs
+	fmt.Println("received more jobs:", ok)
+
+}
