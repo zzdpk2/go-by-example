682ec9cfd0775eae2bc8ffc1c51e3a5d96f632e8
zzdpk2 <wotaicanle@gmail.com>
2025-05-04 04:01:28 +1000

    json marshal unmarshal

---
+++ practice/json/json.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type response1 struct {
+	Page   int
+	Fruits []string
+}
+
+type response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
+
+func main() {
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
+	// fmt.Println(bolB)
+
+	intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
+}
