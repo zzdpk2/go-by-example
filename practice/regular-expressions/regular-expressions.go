26c0de7358628b069ffcaf6c6cddd676c83b015d
zzdpk2 <wotaicanle@gmail.com>
2025-05-03 13:14:40 +1000

    still going

--- practice/regular-expressions/regular-expressions.go
+++ practice/regular-expressions/regular-expressions.go
@@ -1,5 +1,14 @@
 package regular_expressions
 
+import (
+	"fmt"
+	"regexp"
+)
+
 func main() {
+	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
+	fmt.Println(match)
+
+	r, _ := regexp.Compile("p([a-z]+)ch")
 
 }
